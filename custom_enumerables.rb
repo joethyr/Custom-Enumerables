2a9dcae0ae081399592c0c533384dac7d3af703c
joethyr <joethayr@gmail.com>
2021-11-28 21:23:39 -0700

    created custom 'each' enumerable method.

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,5 +1,15 @@
-  module Enumerable
-    def my_each
-      # your code here
+module Enumerable
+  def my_each
+    for number in self do
+      yield(number)
     end
   end
+end
+
+
+
+puts "my_each vs. each"
+numbers = [1, 2, 3, 4, 5]
+numbers.my_each  { |item| puts item }
+puts "\n"
+numbers.each  { |item| puts item }

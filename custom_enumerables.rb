52ac9b529b590325bd545e58656812a21906af96
joethyr <joethayr@gmail.com>
2021-11-28 21:35:24 -0700

    moved code that matches custom method to its counterpart to a seperate file.

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -6,12 +6,3 @@ module Enumerable
   end
 end
 
-puts "my_each vs. each"
-numbers = [1, 2, 3, 4, 5]
-numbers.my_each  { |item| puts item }
-{ 'a' => 1, 'b' => 2 }.my_each { |k, v| puts "#{k}, #{v}" }
-
-
-puts "\n"
-numbers.each  { |item| puts item }
-{ 'a' => 1, 'b' => 2 }.each { |k, v| puts "#{k}, #{v}" }

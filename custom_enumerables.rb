073e034400119f6449536a7eb09c0c33618351ae
joethyr <joethayr@gmail.com>
2021-11-28 21:28:05 -0700

    added code to test if custom each method compatible with hashes.

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,15 +1,17 @@
 module Enumerable
   def my_each
-    for number in self do
-      yield(number)
+    for k, v in self do
+      yield(k, v)
     end
   end
 end
 
-
-
 puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
 numbers.my_each  { |item| puts item }
+{ 'a' => 1, 'b' => 2 }.my_each { |k, v| puts "#{k}, #{v}" }
+
+
 puts "\n"
 numbers.each  { |item| puts item }
+{ 'a' => 1, 'b' => 2 }.each { |k, v| puts "#{k}, #{v}" }

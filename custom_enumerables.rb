e7fc05712ecd13b4c2afb94d1bcf8ff39826717f
joethyr <joethayr@gmail.com>
2021-11-29 20:17:58 -0700

    added custo'each_with_index' method.

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -4,5 +4,13 @@ module Enumerable
       yield(k, v)
     end
   end
+
+  def my_each_with_index
+    iterator = 0
+      for i in self
+        yield i, iterator
+        iterator += 1
+      end
+    end
 end
 
--- match_methods.rb
+++ match_methods.rb
@@ -1,9 +1,20 @@
 require_relative 'custom_enumerables'
 
-puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
+str_arr = %w[Mario Bowser Luigi Peach]
+
+puts "my_each vs. each"
 numbers.my_each  { |item| puts item }
 { 'a' => 1, 'b' => 2 }.my_each { |k, v| puts "#{k}, #{v}" }
 puts "\n"
 numbers.each  { |item| puts item }
 { 'a' => 1, 'b' => 2 }.each { |k, v| puts "#{k}, #{v}" }
+puts "\n"
+
+puts 'my_each_with_index vs each_with_index'
+str_arr.my_each_with_index { |item, index|
+  puts "#{item}: #{index}"}
+puts "\n"
+str_arr.each_with_index { |item, index|
+  puts "#{item}: #{index}"}
+puts "\n"

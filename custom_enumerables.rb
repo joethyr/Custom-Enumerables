6f7224fe11f7949cca3d82f78d13b8de014556c5
joethyr <joethayr@gmail.com>
2021-11-29 20:19:12 -0700

    added code that checks if checks if block is given.

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,11 +1,15 @@
 module Enumerable
   def my_each
+    return to_enum(:my_each) unless block_given?
+
     for k, v in self do
       yield(k, v)
     end
   end
 
   def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
     iterator = 0
       for i in self
         yield i, iterator
